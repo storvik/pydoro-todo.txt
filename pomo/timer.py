6ed5e121650bee3376572d2b674834a48e0fafc1
= <=>
2014-11-23 17:04:36 +0100

    Added seconds

--- pomo/timer.py
+++ pomo/timer.py
@@ -9,10 +9,12 @@ def timer(duration):
         sys.stdout.write("(%s) Remaining: %.0f minutes" % (prog_bar, 24))
         for i in range(duration):
             prog_bar = prog_bar[:i+1].replace(" ", "*") + prog_bar[i+1:]
-            remaining = duration-(i+1)
-            sys.stdout.write("\r(%s) Remaining: %.0f minutes" % (prog_bar, remaining))
-            sys.stdout.flush()
-            time.sleep(60) # Replace with 60 in finished addon
+            remaining_min = duration-(i+1)
+            for sek in range(0,60):
+                remaining_sek = 60 - sek
+                sys.stdout.write("\r(%s) Remaining: %.0f min %.0f sek" % (prog_bar, remaining_min, remaining_sek))
+                sys.stdout.flush()
+                time.sleep(1)
     except KeyboardInterrupt:
         print("\npydoro: Exiting..")
         exit(-1)
